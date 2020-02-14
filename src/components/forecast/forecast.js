1d451be3380086098b166be1f8533187c7d55c99
Vitaliy Shevchenko <vishevchenko@geeksforless.net>
2020-02-14 19:58:52 +0200

    upd forecast component functionality

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -8,21 +8,23 @@ import Loader from "../loader";
 
 const windIcon = <FontAwesomeIcon icon={faWind} />
 
-const buildWeatherTile = (list) => {
+const buildWeatherTiles = (list) => {
     return (
         list.map((item) => {
             const { id, day, time, icon,
                 tempMin, tempMax, desctiption,
                 wind } = item;
             return (
-                <div key={id} className="tile border carousel-item">
-                    {day} <br/>
-                    <img src={icon} alt={desctiption}/> <br/>
-                    {tempMin} &deg;C / {tempMax} &deg;C <br/>
-                    {windIcon} {wind} m/s<br/>
-                    {desctiption} <br/> <hr/>
-                    {time} <br/>
+                <div key={id} className="tile pr-1 m-0 carousel-item">
+                    <div key={id} className="border">
+                        {day} <br />
+                        <img src={icon} alt={desctiption} /> <br />
+                        {tempMin} &deg;C / {tempMax} &deg;C <br />
+                        {windIcon} {wind} m/s<br />
+                        {desctiption} <br /> <hr />
+                        {time} <br />
 
+                    </div>
                 </div>
             )
         })
@@ -34,8 +36,14 @@ export default class Forecast extends Component {
         list: null
     };
 
+    visibleTilesCount = 5;
+    tiles = React.createRef();
+    wrapper = React.createRef();
+
     componentDidUpdate(prev) {
         const { getData, cityId } = this.props;
+
+        this._setTileWidth();
 
         if (prev.cityId == cityId) return;
 
@@ -44,27 +52,76 @@ export default class Forecast extends Component {
                 this.setState({
                     list: data
                 });
-
             });
     }
+
+    _setTileWidth() {
+        const tilesBlock = this.tiles.current;
+
+        if (!tilesBlock) return;
+
+        const tiles = tilesBlock.children;
+
+        if (tiles[0].style.width.length) {
+            return;
+        }
+
+        const { wrapper: { current: wrapper }, visibleTilesCount } = this;
+        const wrapperWidth = wrapper.getBoundingClientRect().width;
+
+        const tileWidth = wrapperWidth / visibleTilesCount;
+
+        [].slice.apply(tiles).forEach(function (tile) {
+            tile.style.setProperty('width', `${tileWidth}px`);
+        });
+    }
+
+    controlLeftClickHandler = () => {
+        const { tiles: { current: tiles}, visibleTilesCount } = this;
+        const leftOffset = tiles.style.getPropertyValue('left');
+        const tileWidth = tiles.firstChild.style.width;
+        let newOffset = (parseInt(leftOffset || 0) + parseInt(tileWidth) * visibleTilesCount);
+       
+        if (newOffset > 0) newOffset = 0;
+        
+        tiles.style.setProperty('left', `${newOffset}px`);
+    };
+
+    constrolRightClickHandler = () => {
+        const { tiles: { current: tiles}, visibleTilesCount } = this;
+        const leftOffset = tiles.style.getPropertyValue('left');
+        const tileWidth = tiles.firstChild.style.width;
+        let newOffset = (parseInt(leftOffset || 0) - parseInt(tileWidth) * visibleTilesCount);
+
+        if (-parseInt(tileWidth) * tiles.childElementCount > newOffset) {
+            newOffset = tileWidth * tiles.children.length;
+        }
+        tiles.style.setProperty('left', `${newOffset}px`);
+    };
 
     render() {
         const { list } = this.state;
 
         if (!list) return <Loader />;
 
-        const tiles = buildWeatherTile(list);
+        const tilesList = buildWeatherTiles(list);
 
         return (
             <div className="carousel slide" data-ride="carousel">
-                <div className="carousel-inner">
-                    {tiles}
+                <div className="inner-wrapper" ref={this.wrapper}>
+                    <div className="carousel-inner" ref={this.tiles}>
+                        {tilesList}
+                    </div>
                 </div>
-                <div className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <div className="carousel-control-prev rounded-circle"
+                    onClick={this.controlLeftClickHandler}
+                    href="#carouselExampleControls" role="button" data-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="sr-only">Previous</span>
                 </div>
-                <div className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <div className="carousel-control-next rounded-circle"
+                    onClick={this.constrolRightClickHandler}
+                    href="#carouselExampleControls" role="button" data-slide="next">
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="sr-only">Next</span>
                 </div>
